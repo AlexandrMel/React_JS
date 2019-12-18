35f22c15f6620b3e5698d4ebb0c283b52fc50cbd
AlexandrMel <melnic.alexandr88@gmail.com>
2019-12-18 09:21:46 +0100

    Starting a React App

---
+++ SPA-todo-app-stage-02-components/src/components/ToDoItem.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function ToDoItem() {
+  return (
+    <div className="item">
+      <p>Clean your face</p>
+      <button className="btn">CHECK</button>
+    </div>
+  );
+}
+
+export default ToDoItem;
