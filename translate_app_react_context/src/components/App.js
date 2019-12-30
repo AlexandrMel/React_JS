59a0bd98e1b8bcfeb15829e86683597011c4b9ee
AlexandrMel <melnic.alexandr88@gmail.com>
2019-12-30 19:47:21 +0100

    Started a new React with Context app

---
+++ translate_app_react_context/src/components/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
+
+class App extends React.Component {
+    state = { language: 'english'}
+onLanguageChange = language => {
+    this.setState({ language })
+}
+
+render(){
+    return (
+    <div className="ui container">
+    
+    <ColorContext.Provider value="primary">
+    <LanguageContext.Provider value={this.state.language}>
+    <UserCreate />
+    </LanguageContext.Provider>
+    </ColorContext.Provider>
+    </div>
+    );
+}
+}
+export default App;
---
+++ translate_app_react_context/src/components/LanguageSelector.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class LanguageSelector extends React.Component {
+    render(){
+<div>Select a language:
+        <i className="flag us" onClick={() => this.props.onLanguageChange('english')}/>
+        <i className="flag nl" onClick={() => this.props.onLanguageChange('dutch')} />
+    </div>
+    }
+}
+
+export default LanguageSelector;
