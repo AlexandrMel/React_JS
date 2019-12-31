85699a1a74c83daadcdd864c17791ad00b3483ae
AlexandrMel <melnic.alexandr88@gmail.com>
2019-12-31 17:07:48 +0100

    Finished the app and made fully reusible hook for the json_api_server

---
+++ hooks_simple_app/src/components/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import ResourceList from './ResourceList';
+import UserList from './UserList';
+
+const App = () => {
+    const [ resource, setResourse] = useState('posts');
+            return ( <div>
+                <UserList />
+                <div>
+                    <button onClick={() => setResourse('posts')}>Posts</button>
+                    <button onClick={() => setResourse('todos')}>Todos</button>
+                </div> 
+                <ResourceList resource={resource} />
+                </div>)
+            };
+
+        export default App;
